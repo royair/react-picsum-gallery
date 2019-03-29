3a13dba6544eca14b37fa1d45575c5b6cd75aeb2
Gilad takoni <gilad1987@gmail.com>
2019-03-29 16:31:01 +0300

    build

---
+++ precache-manifest.f48f079d35448e67512cf681855bd12c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ed8f60fa8c7833ba79d",
+    "url": "/static/js/main.ada9f33d.chunk.js"
+  },
+  {
+    "revision": "65ef72118b599fadc877",
+    "url": "/static/js/2.1afaf1b0.chunk.js"
+  },
+  {
+    "revision": "5ed8f60fa8c7833ba79d",
+    "url": "/static/css/main.43576639.chunk.css"
+  },
+  {
+    "revision": "b4df3ccad5799d2676cd7033371438c1",
+    "url": "/index.html"
+  }
+];
